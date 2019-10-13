4cc8abc93d11bfeaff13a871eecec7e3f3a083ad
panakuma <panakuma@futaba.network>
2019-10-13 19:54:15 +0900

    完成？

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
-from mutagen.mp3 import MP3 as mp3
 import pygame
+import RPi.GPIO as gpio
 
 MUSIC_FILE = "./Anthem_of_the_Soviet_Union.mp3"
 
+gpio.setmode(gpio.BCM)
+gpio.setup(2, gpio.IN)
+
+comment = [""]
+
+
 def main():
     pygame.mixer.init()
-    pygame.mixer.music.load(MUSIC_FILE)
-    music_length = mp3(MUSIC_FILE).info.length
 
+# キーボード操作してたときの名残
+#    while True:
+#        i = input("Type Start or Stop")
+#        if i == "Start" or i == "start":
+#            print("再生")
+#            pygame.mixer.music.play(1)
+#        elif i== "Stop" or i == "stop":
+#            print("停止")
+#            pygame.mixer.music.stop()
+
+    base_status = gpio.HIGH
+    previous_state = gpio.HIGH
     while True:
-        i = input("Type Start or Stop")
-        if i == "Start" or i == "start":
-            print("再生")
+        if gpio.input(2) == gpio.LOW and previous_state != gpio.LOW:
+            print("ソビエトロシアでは、プログラムがあなたを実行する！！")
+            pygame.mixer.music.load(MUSIC_FILE)
             pygame.mixer.music.play(1)
-        elif i== "Stop" or i == "stop":
-            print("停止")
+            previous_state = gpio.LOW
+        elif gpio.input(2) == gpio.HIGH and previous_state == gpio.LOW:
             pygame.mixer.music.stop()
+            pygame.mixer.init()
+            previous_state = gpio.HIGH
+
 
 if __name__ == '__main__':
     main()
